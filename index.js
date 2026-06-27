706424827350ae276c8ccd3295bd87fea05da7fa
gautamchandna5-alt <gautamchandna5@gmail.com>
2026-06-28 03:15:39 +0530

    A simple password generator
    yea I built it as a solo toy project it's functional although it needs some css improvement otherwise you're good to goo

---
+++ index.js
@@ -0,0 +1,28 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
+    "U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
+    "q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","~","`",
+    "!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";",
+    "<",">",".","?","/"];
+let pass1 = document.getElementById("pass1")
+let pass2 = document.getElementById("pass2")
+
+function generateEl(){
+    generateEl1()
+    generateEl2()
+}
+function generateEl1(){
+    let password = ""
+    for (i=0 ; i<15 ; i++){
+        let a = Math.floor(Math.random()*91)
+        password += characters[a]
+    }
+    pass1.textContent = password
+}
+function generateEl2(){
+    let password = ""
+    for (i=0 ; i<15 ; i++){
+        let a = Math.floor(Math.random()*91)
+        password += characters[a]
+    }
+    pass2.textContent = password
+}
